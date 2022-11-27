1fa1214cf6aae1ba18d8b93b374351267b4f7f56
Nguyen Thanh Phuong <phuong.n.t.97.08@gmail.com>
2022-11-28 02:32:43 +0700

    test husky, lint-staged

--- packages/maple/src/lib/maple.ts
+++ packages/maple/src/lib/maple.ts
@@ -2,18 +2,20 @@ import * as express from 'express';
 import { Express, Request, Response } from 'express';
 
 export class Maple {
-  constructor(opt = {}) {}
+  constructor(opt = {}) {
+    // console.log('Maple');
+  }
 
   start() {
-    const app = express();
+    const app: Express = express();
     const port = 3000;
 
     app.get('/', (req: Request, res: Response) => {
-      res.send('Hello World!');
+      res.send('Maple!');
     });
 
     app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+      console.log(`Maple listening on port ${port}`);
     });
   }
 }
