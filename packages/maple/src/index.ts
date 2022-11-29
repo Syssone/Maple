78ca16843c528864b02979523171837c009b3335
Minh <phuong.n.t.97.08@gmail.com>
2022-11-29 12:20:28 +0700

    feat: build to node_modules

--- packages/maple/src/index.ts
+++ packages/maple/src/index.ts
@@ -1,2 +1,6 @@
 import { maple } from './lib/maple';
+import {} from '@maple/utils';
+import resolveCwd = require('resolve-cwd');
+
+console.log(resolveCwd('@maple/utils/index.html'));
 maple({ port: 3000 }).start();
